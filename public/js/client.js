d387887f04face6ea77f8eadef43874f04feaab3
Sebastian Midborn <sebastian.midborn@gmail.com>
2018-02-11 18:26:09 +0100

    Fixed input field background color

--- public/js/client.js
+++ public/js/client.js
@@ -1,10 +1,12 @@
 const socket = io.connect('localhost:1991')
 const send = color => {
   document.getElementsByTagName('BODY')[0].style.background = color
+  document.getElementsByTagName('INPUT')[0].style.background = color
   socket.emit('color', color)
 }
 
 socket.on('response', color => {
   document.getElementsByTagName('BODY')[0].style.background = color
+  document.getElementsByTagName('INPUT')[0].style.background = color
   document.getElementById('picker').value = color
 })
